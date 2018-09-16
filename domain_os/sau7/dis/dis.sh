f31925e6cf0f571da3c816b34b0fbb45ec7247a1
Chris Toshok <toshok@hound.sh>
2018-09-15 20:44:44 -0700

    don't take off the rfc header.  it appears the file is loaded in its entirety at the load address, so the entrypoint is valid against that when we include the header.

--- domain_os/sau7/dis/dis.sh
+++ domain_os/sau7/dis/dis.sh
@@ -6,9 +6,9 @@ D=~/src/domainos-archeology
 load_addr=0x1002000
 dis_addr=$1
 
-rfc_header_bytes=10
+rfc_header_bytes=0
 
 offset=$(node -e "console.log(${dis_addr} - ${load_addr} + ${rfc_header_bytes})")
 
-dd if=${D}/sr10.4-install/sau7/domain_os of=/tmp/domain_os bs=${offset} skip=1
+dd if=${D}/sr10.4-install/sau7/domain_os of=/tmp/domain_os bs=${offset} skip=1 > /dev/null 2>&1
 ${D}/binutils-gdb/binutils/objdump -D --adjust-vma=${dis_addr} -b binary -mm68k:68020 /tmp/domain_os
